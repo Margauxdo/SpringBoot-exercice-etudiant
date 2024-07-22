f6acfddad44866c91858f739124b9053e45a9555
Margaux Doisy <margauxdoisy@gmail.com>
2024-07-22 10:21:03 +0200

    Initial commit

---
+++ src/main/java/org/example/springbootexerciceetudiant/SpringBootExerciceEtudiantApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootexerciceetudiant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExerciceEtudiantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExerciceEtudiantApplication.class, args);
+	}
+
+}
